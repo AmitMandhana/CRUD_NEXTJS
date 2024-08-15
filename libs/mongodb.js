c7e0674d85d205eeef30e5e465ff833f4927a436
Amit Mandhana <amitmandhana1@gmail.com>
2024-08-15 22:01:08 +0530

    updated

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect('mongodb+srv://amitmandhana1:a8013255265@cluster0.mjuvo.mongodb.net');
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
