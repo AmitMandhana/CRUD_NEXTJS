46e5fde5570a07a47a295996a8e293ae16a0fc4a
Amit Mandhana <amitmandhana1@gmail.com>
2024-08-15 22:21:38 +0530

    hi

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -6,7 +6,7 @@ import { HiPencilAlt } from "react-icons/hi";
 // Function to fetch topics from the API
 const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/topics", {
+    const res = await fetch("${process.env.NEXT_PUBLIC_API_URL}/api/topics", {
       cache: "no-store",
     });
 
